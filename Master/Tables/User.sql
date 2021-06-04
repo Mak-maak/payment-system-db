0a5556826175f2dc28e7d7fa80ceadcae93ae15b
mak-maak <azizk7693@gmail.com>
2021-06-04 13:03:16 +0500

    added user table

---
+++ Master/Tables/User.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [Master].[User] (
+    [UserID]    BIGINT        IDENTITY (1, 1) NOT NULL,
+    [FirstName] NVARCHAR (50) NOT NULL,
+    [MiddleName] NVARCHAR (50),
+    [LastName]  NVARCHAR (50) NOT NULL,
+    [ContactNo] VARCHAR (15)  NOT NULL,
+    [CreatedOn] DATETIME      NOT NULL,
+    [CreatedBy] BIGINT        NOT NULL,
+    [IsDeleted] BIT           NOT NULL,
+    PRIMARY KEY CLUSTERED ([UserID] ASC)
+);
+
